ea46275de77cff6a30f16ebd9148cc2999de4e7f
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2019-02-20 11:54:47 +0530

    commit

---
+++ employee_details.php
@@ -0,0 +1,110 @@
+<?php include('header.php'); ?>
+<?php include('sidebar.php'); ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Employee Details
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="index.php"><i class="fa fa-dashboard"></i>Home</a></li>
+            <li class="active">Employee Details</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+        <div class="row">
+
+            <div class="col-md-12">
+                <div class="box box-primary">
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>SI.No</th>
+                                    <th>Employee MCIID</th>
+                                    <th>Designation</th>
+                                    <th>Name</th>
+                                    <th>Mobile Number</th>
+                                    <th>Date Of joining</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>MCI1230</td>
+                                    <td>Developer</td>
+                                    <td>John Deo</td>
+                                    <td>8500xxxxx2</td>
+                                    <td>2012/08/03</td>
+                                    <td>
+                                        <a class="btn btn-danger btn-xs"><i class="fa fa-trash mr-5"></i>Delete</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>MCI1830</td>
+                                    <td>Developer</td>
+                                    <td>John Boscho</td>
+                                    <td>8500xxxxx2</td>
+                                    <td>2012/08/03</td>
+                                    <td>
+                                        <a class="btn btn-danger btn-xs"><i class="fa fa-trash mr-5"></i>Delete</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>MCI1830</td>
+                                    <td>Developer</td>
+                                    <td>John Boscho</td>
+                                    <td>8500xxxxx2</td>
+                                    <td>2012/08/03</td>
+                                    <td>
+                                        <a class="btn btn-danger btn-xs"><i class="fa fa-trash mr-5"></i>Delete</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>MCI1830</td>
+                                    <td>Developer</td>
+                                    <td>John Boscho</td>
+                                    <td>8500xxxxx2</td>
+                                    <td>2012/08/03</td>
+                                    <td>
+                                        <a class="btn btn-danger btn-xs"><i class="fa fa-trash mr-5"></i>Delete</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>MCI1830</td>
+                                    <td>Developer</td>
+                                    <td>John Boscho</td>
+                                    <td>8500xxxxx2</td>
+                                    <td>2012/08/03</td>
+                                    <td>
+                                        <a class="btn btn-danger btn-xs"><i class="fa fa-trash mr-5"></i>Delete</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+
+            </div>
+
+        </div>
+
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php include('footer.php'); ?>
---
+++ new-registration.php
@@ -0,0 +1,304 @@
+<?php include('header.php'); ?>
+<?php include('sidebar.php'); ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            New Student Registration
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="index.php"><i class="fa fa-dashboard"></i>Dashboard</a></li>
+            <li><a href="#">Student Register</a></li>
+            <li class="active">New Registration</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box box-primary">
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <!-- Custom Tabs -->
+                        <form id="example-basic">
+                            <h3>Strength</h3>
+                            <section>
+                                <div class="form-group">
+                                    <label>Strength</label>
+                                    <input type="text" class="form-control" name="" placeholder="Enter Your Students Strengths here..">
+                                </div>
+                            </section>
+                            <h3>Effects</h3>
+                            <section>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>First Name</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter First Name">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Last Name</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Last Name">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Mobile Number</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Mobile Number">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Email Id</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Your Email">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Contact Address</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Address">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Permanent Address</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Address">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Parent & Guardian Details</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Guardian Details">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Documents Upload</label>
+                                            <input type="file" class="form-control" name="">
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                            <h3>Student Course Details</h3>
+                            <section>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>MBBS Score</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Your Score">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>PSAAKET MCI OEE Score</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Your MCI Score">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Suggested Level</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Here..">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Batch</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Batch No">
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                            <h3>Student Accommodation Details</h3>
+                            <section>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Required Accommodation</label>
+                                            <select class="form-control" name="">
+                                                <option value="" selected disabled>Select</option>
+                                                <option value="">Yes</option>
+                                                <option value="">No</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Partnered PG</label>
+                                            <select class="form-control" name="">
+                                                <option value="" selected disabled>Select</option>
+                                                <option value="">Option</option>
+                                                <option value="">Option</option>
+                                                <option value="">Option</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Floor No</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Floor No">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Room No</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Room No">
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                            <h3>Fee Details</h3>
+                            <section>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Opted Level</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Level">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Course Fee</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Course Fee">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Registration Fee</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Registration Fee">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Accommodation Fee</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Accommodation Fee">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Batch Discount</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Discount">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>GST</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter GST">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Total Amount</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Total Amount">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Payable Amount</label>
+                                            <input type="text" class="form-control" name="" placeholder="Enter Payable Amount">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Payment Mode</label>
+                                            <select class="form-control" name="">
+                                                <option value="" selected disabled>Select</option>
+                                                <option value="">Pay through online</option>
+                                                <option value="">Cash</option>
+                                                <option value="">DD / Deposit</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                            <h3>Verify & Confirm</h3>
+                            <section>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="table-responsive">
+                                            <table id="" class="table table-bordered table-striped">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Student Name</th>
+                                                        <th>Mobile Number</th>
+                                                        <th>Email ID</th>
+                                                        <th>Contact Address</th>
+                                                        <th>Permanent Address</th>
+                                                        <th>Guardian Details</th>
+                                                        <th>MBBS Score</th>
+                                                        <th>PSAAKET MCI Score</th>
+                                                        <th>Suggested Level</th>
+                                                        <th>Batch No</th>
+                                                        <th>Accommodation</th>
+                                                        <th>PG</th>
+                                                        <th>Floor No</th>
+                                                        <th>Room No</th>
+                                                        <th>Opted Level</th>
+                                                        <th>Course Fee</th>
+                                                        <th>Registration Fee</th>
+                                                        <th>Accommodation Fee</th>
+                                                        <th>Discount</th>
+                                                        <th>GST</th>
+                                                        <th>Total Amount</th>
+                                                        <th>Payable Amount</th>
+                                                        <th>Payment Mode</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                        <td>xxxxxxx</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                        </form>
+                        <!-- nav-tabs-custom -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php include('footer.php'); ?>
+
+<script>
+    $("#example-basic").steps({
+        headerTag: "h3",
+        bodyTag: "section",
+        transitionEffect: "slideLeft",
+        autoFocus: true
+    });
+</script>
